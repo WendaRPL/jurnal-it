2c6710371be5ae915360954bea53d06112d905ff
WendaRPL <danurwenda.sulistya@gmail.com>
2025-08-14 11:39:14 +0700

    Add files via upload

---
+++ jurnal-it/modules/layout/sections.php
@@ -0,0 +1,40 @@
+<?php
+function renderUnfulfilledSection() {
+?>
+<section class="unfulfilled-section">
+  <div class="section-header">Belum Terpenuhi</div>
+  <div class="user-cards">
+    <div class="user-card">
+      <div class="user-header">Nama User</div>
+      <div class="user-date">10 Aug 2025</div>
+      <div class="user-date">08 Aug 2025</div>
+      <div class="user-date">07 Aug 2025</div>
+      <div class="user-date">04 Aug 2025</div>
+    </div>
+    <div class="user-card">
+      <div class="user-header">Nama User</div>
+      <div class="user-date">09 Aug 2025</div>
+      <div class="user-date">07 Aug 2025</div>
+      <div class="user-date">06 Aug 2025</div>
+    </div>
+    <div class="user-card">
+      <div class="user-header">Nama User</div>
+      <div class="user-date">07 Aug 2025</div>
+      <div class="user-date">03 Aug 2025</div>
+    </div>
+    <div class="user-card">
+      <div class="user-header">Nama User</div>
+      <div class="user-date">09 Aug 2025</div>
+      <div class="user-date">08 Aug 2025</div>
+      <div class="user-date">06 Aug 2025</div>
+      <div class="user-date">05 Aug 2025</div>
+    </div>
+    <div class="user-card">
+      <div class="user-header">Nama User</div>
+      <div class="user-date">10 Aug 2025</div>
+    </div>
+  </div>
+</section>
+<?php
+}
+?>
