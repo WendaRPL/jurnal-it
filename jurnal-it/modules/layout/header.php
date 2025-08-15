0a1408c0b509d9184fe92abb45f99940afdf3fae
WendaRPL <danurwenda.sulistya@gmail.com>
2025-08-15 14:15:40 +0700

    Create header.php

---
+++ jurnal-it/modules/layout/header.php
@@ -0,0 +1,23 @@
+<?php
+function renderHeader($title = "Jurnal IT") {
+?>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title><?php echo htmlspecialchars($title); ?></title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+
+  <!-- Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="modules/css/color.css">
+  <link rel="stylesheet" href="modules/css/style.css">
+
+</head>
+<body>
+<?php
+}
+?>
