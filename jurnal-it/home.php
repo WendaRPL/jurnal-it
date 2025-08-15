d255d20d4721e8f1715d7a259470b7c2776aa877
WendaRPL <danurwenda.sulistya@gmail.com>
2025-08-15 14:17:02 +0700

    Update home.php

--- jurnal-it/home.php
+++ jurnal-it/home.php
@@ -1,8 +1,9 @@
 <?php
+session_start();
 require_once "modules/layout/header.php";
 require_once "modules/layout/navbar.php";
-require_once "modules/layout/cards.php";
-require_once "modules/layout/sections.php";
+require_once "cards.php";
+require_once "sections.php";
 require_once "modules/layout/footer.php";
 
 renderHeader("Home - Jurnal IT");
