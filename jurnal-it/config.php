5772814e34be06e3521ad0cfbc1d08f4c52d4624
WendaRPL <danurwenda.sulistya@gmail.com>
2025-08-18 10:51:59 +0700

    Update config.php

--- jurnal-it/config.php
+++ jurnal-it/config.php
@@ -6,6 +6,23 @@ $password = "";
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
+if (isset($_SESSION['user_id'])) {
+    $uid = $_SESSION['user_id'];
+
+    // Update last_online_datetime tiap kali ada aktivitas
+    $stmt = $conn->prepare("UPDATE User SET last_online_datetime = NOW() WHERE id = ?");
+    $stmt->bind_param("i", $uid);
+    $stmt->execute();
+    $stmt->close();
+
+    // Update status jadi online
+    $stmt = $conn->prepare("UPDATE User SET status = 'online' WHERE id = ?");
+    $stmt->bind_param("i", $uid);
+    $stmt->execute();
+    $stmt->close();
+}
+
+
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
